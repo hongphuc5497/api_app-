051fe74df28c894d65aa05cd9181899c348eeec6
hongphuc <hongphuc.dthp@gmail.com>
2019-10-10 07:42:54 +0700

    Updated

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module ApiApp
       end
     end
 
-    config.middleware.user Rack::Attack 
+    config.middleware.use Rack::Attack 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.

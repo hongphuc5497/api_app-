3e3025fd138f992469787137b957136a94eac3e6
hongphuc <hongphuc.dthp@gmail.com>
2019-10-10 07:57:14 +0700

    Authenticating API

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_125752) do
+ActiveRecord::Schema.define(version: 2019_10_10_004325) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "api_key"
   end
 
 end
